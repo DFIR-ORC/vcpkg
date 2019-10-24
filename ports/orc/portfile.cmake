a624942a064dac6fb7fe507d02265e19b63121ac
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2019-10-24 17:12:17 +0200

    [orc] Add patch to use http instead of ftp

--- ports/orc/portfile.cmake
+++ ports/orc/portfile.cmake
@@ -7,7 +7,7 @@ vcpkg_from_github(
     PATCHES
       0003-dependencies-from-vcpkg.patch
       0004-update-tzdata.patch
-      0005-disable-tzdata.patch
+      0005-replace-ftp-with-http-for-tzdata.patch
 )
 
 file(REMOVE "${SOURCE_PATH}/cmake_modules/FindGTest.cmake")
