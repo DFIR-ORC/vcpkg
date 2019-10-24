80dd0c074ac1621eb82291b38ef5385045ffaa2c
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2019-10-24 17:12:32 +0200

    [7zip] Make dll exports public

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -13,6 +13,7 @@ vcpkg_extract_source_archive(
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/7zip-config.cmake.in" DESTINATION "${SOURCE_PATH}")
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/Archive2.def DESTINATION "${SOURCE_PATH}/CPP/7zip/Archive/")
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
