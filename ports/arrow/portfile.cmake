82ff3cd6eb8a7e7ace953d51324c44b8ec68ccff
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2019-12-06 16:46:41 +0100

    [arrow] fix build with 0.15.1

--- ports/arrow/portfile.cmake
+++ ports/arrow/portfile.cmake
@@ -13,8 +13,6 @@ vcpkg_from_github(
     PATCHES
         all.patch
         fix-msvc-1900.patch
-        fix-cpp17-build.patch
-        fix-vs2019_v141-build.patch
 )
 
 string(COMPARE EQUAL ${VCPKG_LIBRARY_LINKAGE} "dynamic" ARROW_BUILD_SHARED)
