553f7f0cc4cb198b78fbf4da47d4cfd11a6e8251
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2019-12-11 11:50:13 +0100

    [libressl] Fix output binaries name to match openssl

--- ports/libressl/portfile.cmake
+++ ports/libressl/portfile.cmake
@@ -18,6 +18,7 @@ vcpkg_extract_source_archive(
     PATCHES
         0001-enable-ocspcheck-on-msvc.patch
         0002-suppress-msvc-warnings.patch
+        0003-dfirorc-fix_output_name.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
