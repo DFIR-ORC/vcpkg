7d724a8703e79c209a3e793d4e80a6a1f494dd24
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2020-12-17 19:07:48 +0100

    [liblzma] Add XP support

--- ports/liblzma/portfile.cmake
+++ ports/liblzma/portfile.cmake
@@ -8,6 +8,7 @@ vcpkg_from_github(
         enable-uwp-builds.patch
         fix_config_include.patch
         win_output_name.patch # Fix output name on Windows. Autotool build does not generate lib prefixed libraries on windows. 
+        xp.patch
 )
 
 vcpkg_configure_cmake(
