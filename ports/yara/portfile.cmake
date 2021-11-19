b51e33449a115e8f24a02f5e9919607865257ef3
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2021-11-19 14:27:48 +0100

    [yara] Update to 4.1.3

--- ports/yara/portfile.cmake
+++ ports/yara/portfile.cmake
@@ -3,9 +3,10 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 vcpkg_from_github(
   OUT_SOURCE_PATH SOURCE_PATH
   REPO VirusTotal/yara
-  REF v4.0.2
-  SHA512 22575cb7b48eead3b5051ce13beb3deef88a1b605de20ec8e5fc2651c0dd0009eb6c0ecddb8600a43a757ceb6ea298e94f71a26d2f9acb31a73830e18c81d10c
+  REF v4.1.3
+  SHA512 1bfa1787c62dfd9a87fa8db5e8c2fa68f082ae66b16b5373bdcc6bc66b32016fcaffd4baa7e59a7c1f6d3426c972eca9cc22f70d475067d7557b1014a4ab65fc
   HEAD_REF dev
+  PATCHES xp_sp2_target.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
