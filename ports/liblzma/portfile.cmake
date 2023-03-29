fff546d9f1e66a444bc40df601d6bf19119d4ba6
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2023-03-29 16:04:01 +0200

    [liblzma] Add XP support

--- ports/liblzma/portfile.cmake
+++ ports/liblzma/portfile.cmake
@@ -9,6 +9,7 @@ vcpkg_from_sourceforge(
         win_output_name.patch # Fix output name on Windows. Autotool build does not generate lib prefixed libraries on windows. 
         add_support_ios.patch # add install bundle info for support ios 
         build-tools.patch
+        xp.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
