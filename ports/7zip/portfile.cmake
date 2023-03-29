8c719ba52e3f1aec720d3a5b159d36ef68ebeb9e
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2023-03-29 16:09:22 +0200

    [7zip] Add support for static linking
    Some singletons need to be instanciated to avoid being
    optimized out when linking.

--- ports/7zip/portfile.cmake
+++ ports/7zip/portfile.cmake
@@ -9,6 +9,8 @@ vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${ARCHIVE}"
     NO_REMOVE_ONE_LEVEL
+    PATCHES
+        add-functions-and-fixes-for-static-link.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
@@ -33,6 +35,7 @@ file(
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(
     COPY
+        ${CMAKE_CURRENT_LIST_DIR}/7zip.h
         ${CMAKE_CURRENT_LIST_DIR}/guids.h
     DESTINATION ${CURRENT_PACKAGES_DIR}/include/7zip
 )
