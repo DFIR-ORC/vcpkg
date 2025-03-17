08352492a2ede707f7e6f617f8fb3fe6cf1281fb
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2025-03-17 12:56:30 +0100

    [libressl] Update to 3.9.2

--- ports/libressl/portfile.cmake
+++ ports/libressl/portfile.cmake
@@ -1,29 +1,27 @@
 if(EXISTS "${CURRENT_INSTALLED_DIR}/include/openssl/ssl.h")
-  message(WARNING "Can't build libressl if openssl is installed. Please remove openssl, and try install libressl again if you need it. Build will continue since libressl is a subset of openssl")
-  set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
-  return()
+    message(FATAL_ERROR "Can't build libressl if openssl is installed. Please remove openssl, and try install libressl again if you need it.")
 endif()
 
 vcpkg_download_distfile(
     LIBRESSL_SOURCE_ARCHIVE
     URLS "https://ftp.openbsd.org/pub/OpenBSD/LibreSSL/${PORT}-${VERSION}.tar.gz"
-         "https://ftp.fau.de/openbsd/LibreSSL/${PORT}-${VERSION}.tar.gz"
+         "https://github.com/libressl/portable/releases/download/v${VERSION}/${PORT}-${VERSION}.tar.gz"
     FILENAME "${PORT}-${VERSION}.tar.gz"
-    SHA512 8fc81e05d1c9f9259d06508ca97d5a1ba5d46b857088c273c20e6b242921f7eac58a1136564ad9831c923758ee63f7b0897c8c6c7b1e53ab8132a995cc559aeb
+    SHA512 b7b9f47c77fd27787b7c7ae7e78cd831fe9f7f32e280f54952994569bfe69ff03022e349aea9ea734c50b079693c6e15a3c1115ef0093e523437904074da5784
 )
 
+# 0003-dfirorc-fix_output_name.patch
+# 0004-dfirorc-timegm_conflict.patch
 vcpkg_extract_source_archive(
     SOURCE_PATH
     ARCHIVE "${LIBRESSL_SOURCE_ARCHIVE}"
     PATCHES
-        0001-enable-ocspcheck-on-msvc.patch
-        0002-suppress-msvc-warnings.patch
-        0003-dfirorc-fix_output_name.patch
-        0004-dfirorc-timegm_conflict.patch
-        0005-dfirorc-50_xp_api.patch
-        0006-dfirorc-50_xp_api_shared_crt.patch
-        0007-dfirorc-xp_inet_pton.patch
-        0008-dfirorc-xp_bcrypt_gen_random.patch
+    0000-dfirorc-missing-ssize_t.patch
+    0001-dfirorc-pkgconfig.patch
+    0005-dfirorc-50_xp_api.patch
+    0006-dfirorc-50_xp_api_shared_crt.patch
+    0007-dfirorc-xp_inet_pton.patch
+    0008-dfirorc-xp_bcrypt_gen_random.patch
 )
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/inet_pton.c DESTINATION "${SOURCE_PATH}/crypto/compat/")
@@ -38,42 +36,42 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         ${FEATURE_OPTIONS}
+        -DLIBRESSL_INSTALL_CMAKEDIR=share/${PORT}
         -DLIBRESSL_TESTS=OFF
     OPTIONS_DEBUG
         -DLIBRESSL_APPS=OFF
 )
 
 vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+vcpkg_fixup_pkgconfig()
+#vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libcrypto.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libssl.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/libtls.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/lib/pkgconfig/openssl.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libcrypto.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libssl.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/libtls.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/openssl.pc" "${CURRENT_PACKAGES_DIR}" "\${prefix}")
+
+# libressl as openssl replacement
+configure_file("${CURRENT_PORT_DIR}/vcpkg-cmake-wrapper.cmake.in" "${CURRENT_PACKAGES_DIR}/share/openssl/vcpkg-cmake-wrapper.cmake" @ONLY)
 
 if("tools" IN_LIST FEATURES)
     vcpkg_copy_tools(TOOL_NAMES ocspcheck openssl DESTINATION "${CURRENT_PACKAGES_DIR}/tools/openssl" AUTO_CLEAN)
 endif()
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    file(REMOVE_RECURSE
-        "${CURRENT_PACKAGES_DIR}/bin"
-        "${CURRENT_PACKAGES_DIR}/debug/bin"
-    )
-endif()
 file(REMOVE_RECURSE
     "${CURRENT_PACKAGES_DIR}/etc/ssl/certs"
     "${CURRENT_PACKAGES_DIR}/debug/etc/ssl/certs"
-    "${CURRENT_PACKAGES_DIR}/share/man"
     "${CURRENT_PACKAGES_DIR}/debug/include"
     "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/share/man"
 )
 
-vcpkg_copy_pdbs()
-
-vcpkg_install_copyright(FILE_LIST
-    "${SOURCE_PATH}/COPYING"
-    "${CMAKE_CURRENT_LIST_DIR}/inet_pton.COPYING"
-)
-
-if(VCPKG_TARGET_IS_WINDOWS OR VCPKG_TARGET_IS_UWP)
-    file(GLOB_RECURSE LIBS "${CURRENT_PACKAGES_DIR}/*.lib")
-    foreach(LIB ${LIBS})
-        string(REGEX REPLACE "(.+)-[0-9]+\\.lib" "\\1.lib" LINK "${LIB}")
-        execute_process(COMMAND "${CMAKE_COMMAND}" -E copy "${LIB}" "${LINK}")
-    endforeach()
-endif()
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
