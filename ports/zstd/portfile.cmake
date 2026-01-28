7e7248b20fead8479a8207a4ca7965500c803768
fabienfl <fabien.fl-orc@ssi.gouv.fr>
2026-01-28 19:49:33 +0100

    [zstd] Update to version 1.5.7

--- ports/zstd/portfile.cmake
+++ ports/zstd/portfile.cmake
@@ -2,21 +2,21 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO facebook/zstd
     REF "v${VERSION}"
-    SHA512 b6f64850ceb6cfed831fff3c43508d2a72338862a96dd9430b1d3ebbfcee40201c8b6dcf8b6b603e252bb96f3f283c9cb07da7f24414187f5f1fea3b51e01863
+    SHA512 26e441267305f6e58080460f96ab98645219a90d290a533410b1b0b1d2f870721c95f8384e342ee647c5e968385a5b7e30c2d04340c37f59b3e6d86762c3260c
     HEAD_REF dev
     PATCHES
         no-static-suffix.patch
         fix-emscripten-and-clang-cl.patch
+        fix-windows-rc-compile.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ZSTD_BUILD_STATIC)
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ZSTD_BUILD_SHARED)
 
-if(VCPKG_TARGET_IS_WINDOWS)
-    # Enable multithreaded mode. CMake build doesn't provide a multithreaded
-    # library target, but it is the default in Makefile and VS projects.
-    set(VCPKG_C_FLAGS "${VCPKG_C_FLAGS}")
-    set(VCPKG_CXX_FLAGS "${VCPKG_CXX_FLAGS}")
+if("tools" IN_LIST FEATURES)
+   set(ZSTD_BUILD_PROGRAMS 1)
+else()
+   set(ZSTD_BUILD_PROGRAMS 0)
 endif()
 
 vcpkg_cmake_configure(
@@ -25,10 +25,13 @@ vcpkg_cmake_configure(
         -DZSTD_BUILD_SHARED=${ZSTD_BUILD_SHARED}
         -DZSTD_BUILD_STATIC=${ZSTD_BUILD_STATIC}
         -DZSTD_LEGACY_SUPPORT=1
-        -DZSTD_BUILD_PROGRAMS=0
         -DZSTD_BUILD_TESTS=0
         -DZSTD_BUILD_CONTRIB=0
-        -DZSTD_MULTITHREAD_SUPPORT=0
+        -DZSTD_MULTITHREAD_SUPPORT=1
+    OPTIONS_RELEASE
+        -DZSTD_BUILD_PROGRAMS=${ZSTD_BUILD_PROGRAMS}
+    OPTIONS_DEBUG
+        -DZSTD_BUILD_PROGRAMS=OFF
 )
 
 vcpkg_cmake_install()
@@ -50,6 +53,10 @@ if(VCPKG_TARGET_IS_WINDOWS AND VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
     foreach(HEADER IN ITEMS zdict.h zstd.h zstd_errors.h)
         vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/${HEADER}" "defined(ZSTD_DLL_IMPORT) && (ZSTD_DLL_IMPORT==1)" "1" )
     endforeach()
+endif()
+
+if(VCPKG_TARGET_IS_WINDOWS AND ZSTD_BUILD_PROGRAMS)
+    vcpkg_copy_tools(TOOL_NAMES zstd AUTO_CLEAN)
 endif()
 
 file(INSTALL "${CMAKE_CURRENT_LIST_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
